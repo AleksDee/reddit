06295c85b1cd2e94386e693a6f7d16e61e426cf9
Dmitry <dmitry.aleksandrin@gmail.com>
2021-08-25 23:45:54 +0300

    добавил graphql

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
 import   { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
-import microConfig from './mikro-orm.config'
+// import { Post } from './entities/Post';
+import microConfig from './mikro-orm.config';
+import express from "express";
+import {ApolloServer} from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    const post = orm.em.create(Post, {title: 'my first post'})
-    await orm.em.persistAndFlush(post);
-    await orm.em.nativeInsert(Post, {title: 'my first post 2'})
+    await orm.getMigrator().up();
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(4000, () => {
+        console.log("server started om localhost:4000");
+    })
+    // const post = orm.em.create(Post, {title: 'my first post'})
+    // await orm.em.persistAndFlush(post);
 }
 
 main().catch(err => {
